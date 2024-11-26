45f67c650d221809e7a87bafe2218631fab1859b
zuzihe <dya_yaaa@naver.com>
2024-11-26 22:43:47 +0900

    홈 화면 UI 완성 - 아이콘 png 추가, 글쓰기 버튼 기능 수정

--- lib/mainPages/HomePage.dart
+++ lib/mainPages/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../screens/WritePage.dart';
+import '../theme/colors.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,58 +11,69 @@ class _HomePageState extends State<HomePage> {
   final List<String> tags = ['전체', '원스톱', '학식당', '학술정보관', '상상빌리지', '상상관'];
 
   String selectedTag = '전체';
+  bool isPopupVisible = false; // 팝업 표시 여부
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2, // 탭의 개수: 습득물, 분실물
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white, // AppBar의 배경색을 흰색으로 설정
-          title: Row(
-            children: [
-              Image.asset(
-                'assets/logo.png',
-                height: 40,
+      length: 2,
+      child: Stack(
+        children: [
+          Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              title: Row(
+                children: [
+                  Image.asset(
+                    'assets/logo.png',
+                    height: 35,
+                  ),
+                ],
               ),
-            ],
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                print('검색 아이콘 클릭');
-              },
+              actions: [
+                IconButton(
+                  icon: Image.asset('assets/icons/ic_search.png', height: 20),
+                  onPressed: () {
+                    print('검색 아이콘 클릭');
+                  },
+                ),
+                IconButton(
+                  icon: Image.asset('assets/icons/ic_notification.png', height: 20),
+                  onPressed: () {
+                    print('알림 아이콘 클릭');
+                  },
+                ),
+                IconButton(
+                  icon: Image.asset('assets/icons/ic_user.png', height: 20),
+                  onPressed: () {
+                    print('유저 아이콘 클릭');
+                  },
+                ),
+              ],
+              bottom: TabBar(
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                indicatorColor: Color(0xFF042D6F),
+                labelStyle: TextStyle(
+                  fontFamily: 'Neo',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+                unselectedLabelStyle: TextStyle(
+                  fontFamily: 'Neo',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                ),
+                tabs: [
+                  Tab(text: '습득물'),
+                  Tab(text: '분실물'),
+                ],
+              ),
             ),
-            IconButton(
-              icon: Icon(Icons.notifications), // 알림 버튼
-              onPressed: () {
-                print('알림 아이콘 클릭');
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.person),
-              onPressed: () {
-                print('유저 아이콘 클릭');
-              },
-            ),
-          ],
-          bottom: TabBar(
-            labelColor: Colors.black, // 탭 활성 상태 텍스트 색상
-            unselectedLabelColor: Colors.grey, // 비활성 탭 텍스트 색상
-            indicatorColor:Color(0xFF042D6F), // 탭 아래 선택된 표시줄 색상
-            tabs: [
-              Tab(text: '습득물'), // 첫 번째 탭
-              Tab(text: '분실물'), // 두 번째 탭
-            ],
-          ),
-        ),
-        body: Stack(
-          children: [
-            Column(
+            body: Column(
               children: [
+                SizedBox(height: 10),
                 _buildTagSelector(),
-                Divider(), // 구분선
                 Expanded(
                   child: TabBarView(
                     children: [
@@ -72,53 +84,143 @@ class _HomePageState extends State<HomePage> {
                 ),
               ],
             ),
-            // 글쓰기 버튼 고정
-            Positioned(
-              bottom: 20, // 화면 아래에서 20px 위치
-              right: 20, // 화면 오른쪽에서 20px 위치
-              child: FloatingActionButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => WritePage()),
-                  );
+          ),
+          if (isPopupVisible)
+            Positioned.fill(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isPopupVisible = false; // 배경 클릭 시 팝업 닫기
+                  });
                 },
-                backgroundColor: Color(0xFF042D6F), // 버튼 배경색
-                child: Icon(Icons.edit, color: Colors.white), // 버튼 아이콘
+                child: Container(
+                  color: Colors.black.withOpacity(0.5), // 화면 전체 반투명 어두운 배경
+                ),
               ),
             ),
-          ],
-        ),
+          Positioned(
+            bottom: 26,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: isPopupVisible
+                  ? _buildPopupButtons() // 팝업 버튼 표시
+                  : ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isPopupVisible = true; // 팝업 활성화
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF042D6F),
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: Text(
+                  '글쓰기',
+                  style: TextStyle(
+                    fontFamily: 'Neo',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 13,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  // 장소 태그 선택 UI
+  Widget _buildPopupButtons() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              isPopupVisible = false; // 팝업 닫기
+            });
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => WritePage()),
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color(0xFF042D6F),
+            padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+          child: Text(
+            '습득물',
+            style: TextStyle(
+              fontFamily: 'Neo',
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 13,
+            ),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              isPopupVisible = false; // 팝업 닫기
+            });
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => WritePage()),
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color(0xFF042D6F),
+            padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+          child: Text(
+            '분실물',
+            style: TextStyle(
+              fontFamily: 'Neo',
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 13,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget _buildTagSelector() {
     return SingleChildScrollView(
-      scrollDirection: Axis.horizontal, // 태그를 가로 스크롤 가능하게 설정
+      scrollDirection: Axis.horizontal,
       child: Row(
         children: tags.map((tag) {
           final bool isSelected = selectedTag == tag;
           return GestureDetector(
             onTap: () {
               setState(() {
-                selectedTag = tag; // 선택된 태그 업데이트
+                selectedTag = tag;
               });
             },
             child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 6), // 태그 간 간격
-              padding: EdgeInsets.symmetric(horizontal: 9, vertical: 6), // 태그 내부 간격
+              margin: EdgeInsets.symmetric(horizontal: 4),
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
               decoration: BoxDecoration(
-                color: isSelected ? Color(0xFF042D6F) : Colors.white, // 선택 상태에 따른 배경색
-                borderRadius: BorderRadius.circular(14), // 둥근 모서리
-
+                color: isSelected ? Color(0xFF042D6F) : ColorStyles.seedColor,
+                borderRadius: BorderRadius.circular(14),
               ),
               child: Text(
                 tag,
                 style: TextStyle(
-                  color: isSelected ? Colors.white : Colors.black, // 선택 상태에 따른 텍스트 색상
-                  fontSize: 14,
+                  color: isSelected ? Colors.white : Colors.black,
+                  fontSize: 12,
                 ),
               ),
             ),
