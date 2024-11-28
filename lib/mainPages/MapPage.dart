bfcc94e78c61d2a543e7d8f5f21ea40d79084e55
zuzihe <dya_yaaa@naver.com>
2024-11-28 23:08:27 +0900

    MapPage 분실물 보관 장소에 부기 마커 추가

--- lib/mainPages/MapPage.dart
+++ lib/mainPages/MapPage.dart
@@ -11,10 +11,19 @@ class _MapPageState extends State<MapPage> {
   final TransformationController _transformationController =
   TransformationController();
 
+  // 핀의 위치 목록 (Offset으로 좌표 지정)
+  final List<Offset> pins = [
+    Offset(290, 250), // 원스톱
+    Offset(350, 380), // 학식당
+    Offset(340, 280), // 학술정보관
+    Offset(200, 350), // 상상파크
+    Offset(240, 500), // 상상빌리지
+  ];
+
   @override
   void initState() {
     super.initState();
-    // 초기 확대 비율 설정 (2배 확대)
+    // 초기 확대 비율 설정 (1.5배 확대)
     _transformationController.value = Matrix4.identity()..scale(1.5);
   }
 
@@ -31,12 +40,48 @@ class _MapPageState extends State<MapPage> {
             maxScale: maxScale,
             constrained: false,
             transformationController: _transformationController,
-            child: Center(
-              child: Image.asset(
-                'assets/map.png',
-                height: screenHeight,
-                fit: BoxFit.fitHeight,
-              ),
+            child: Stack(
+              children: [
+                // 지도 이미지
+                Center(
+                  child: Image.asset(
+                    'assets/map.png',
+                    height: screenHeight,
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+                // 핀들 추가
+                ...pins.map((pin) {
+                  return Positioned(
+                    left: pin.dx, // X 좌표
+                    top: pin.dy, // Y 좌표
+                    child: GestureDetector(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (_) => AlertDialog(
+                            title: Text('핀 정보'),
+                            content: Text('핀 위치: (${pin.dx}, ${pin.dy})'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('닫기'),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      child: Image.asset(
+                        'assets/icons/ic_pin.png',
+                        width: 32, // 핀 이미지의 너비
+                        height: 45, // 핀 이미지의 높이
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ],
             ),
           );
         },
